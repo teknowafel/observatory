4613624a2ad2729124bdb761b578714224d43049
teknowafel <samnorman1572@gmail.com>
2022-06-29 17:29:54 -0400

    url input not working for some reason

--- frontend/modules/pihole/pihole.js
+++ frontend/modules/pihole/pihole.js
@@ -3,17 +3,21 @@ fetch('modules/pihole/template.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById("container-main").innerHTML += data;
+
+        //document.getElementById("pihole-url").value = localStorage.getItem("pihole-url");
+        document.getElementById("pihole-url").value = "poop";
         getPihole();
         setInterval(() => {
             getPihole();
         }, 5000);
-
+        
         importNext();
     });
 
 // Get Pi-Hole data
 const getPihole = () => {
-    fetch('http://192.168.1.67/admin/api.php')
+    localStorage.setItem("pihole-url", document.getElementById("pihole-url").value);
+    fetch(`${localStorage.getItem("pihole-url")}/admin/api.php`)
         .then(response => response.json())
         .then(data => {
             document.getElementById("pihole-adlist-domains").innerHTML = data.domains_being_blocked;
