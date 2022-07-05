ef5ef77748bc40348c529e865fcb99d422fd3565
sam <snorman19@comcast.net>
2022-07-04 22:44:58 -0400

    docker works except for /sys/class/net

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM debian:bullseye-slim
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    nodejs \
+    npm \
+    net-tools \
+    smartmontools \
+    procps
+
+RUN npm i -g n && \
+    n 18
+
+RUN mkdir -p /etc/apt/keyrings
+
+RUN curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+RUN echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+RUN apt-get update && apt-get install -y docker-ce-cli docker-compose-plugin
+
+COPY . .
+
+RUN npm i .
+
+ENTRYPOINT [ "npm", "start" ]
--- backend/modules/networks.js
+++ backend/modules/networks.js
@@ -5,11 +5,21 @@ module.exports = (app) => {
         let interfaces = []; // Create an array to hold network interfaces
         fs.readdir("/sys/class/net", (error, files) => { // Read the directory of networks
             files.forEach(interface => { // Iterate through the networks
+                let rxBytes = "";
+                let txBytes = "";
+                // Read the rx and tx bytes with readFileSync like in the lower part of the code, but using try/catch
+                try {
+                    rxBytes = fs.readFileSync("/sys/class/net/" + interface + "/statistics/rx_bytes").toString().trim();
+                    txBytes = fs.readFileSync("/sys/class/net/" + interface + "/statistics/tx_bytes").toString().trim();
+                } catch (error) {
+                    console.log(`Error reading network stats for network ${interface}`);
+                }
+
                 interfaces.push( // Add the interface to the array
                     {
                         "name": interface, // Name of the interface
-                        "rxBytes": fs.readFileSync(`/sys/class/net/${interface}/statistics/rx_bytes`).toString().trim(), // Bytes recieved
-                        "txBytes": fs.readFileSync(`/sys/class/net/${interface}/statistics/tx_bytes`).toString().trim() // Bytes transmitted
+                        "rxBytes": rxBytes, // Bytes recieved
+                        "txBytes": txBytes // Bytes transmitted
                     }
                 )
             });
