421d61b7760dfb9a4943df39c4d601935f38b290
teknowafel <samnorman1572@gmail.com>
2022-07-15 09:31:11 -0400

    many optimizations and features

--- backend/modules/networks.js
+++ backend/modules/networks.js
@@ -4,7 +4,7 @@ const tableparser = require("table-parser"); // We need table-parser to parse th
 module.exports = (app) => {
     app.get("/networks", (req, res) => { // Networks endpoint to read network statistics
         let interfaces = []; // Create an array to hold network interfaces
-        sh("netstat -i").result((results) => { // Read the output of netstat
+        sh(`chroot /host "netstat" "-i"`).result((results) => { // Read the output of netstat
             table = tableparser.parse(results); // Parse the output of netstat
             table.splice(0, 2); // Remove the first two rows from the output
             table.forEach(interface => { // Iterate through the networks
